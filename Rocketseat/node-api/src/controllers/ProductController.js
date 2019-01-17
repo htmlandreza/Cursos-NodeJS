41541c2745b1a91f669c2e3c16fbe50b08c6381c
Andreza Moreira <htmlandreza@gmail.com>
2019-01-17 16:13:25 -0200

    Operações

---
+++ Rocketseat/node-api/src/controllers/ProductController.js
@@ -0,0 +1,13 @@
+// Importando
+const mongoose = require("mongoose");
+
+// Importando Model
+const Product = mongoose.model('Product');
+
+// Objeto
+module.exports = {
+    async index(req, res){
+        //listagem de dados
+
+    },
+};
