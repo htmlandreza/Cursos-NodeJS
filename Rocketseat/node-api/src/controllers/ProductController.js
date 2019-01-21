af1ab5794fcfd52e8cd8e285f66a5aed5bb31061
Andreza Moreira <htmlandreza@gmail.com>
2019-01-21 14:08:32 -0200

    Adicionando o index, show, store, update e destoy

--- Rocketseat/node-api/src/controllers/ProductController.js
+++ Rocketseat/node-api/src/controllers/ProductController.js
@@ -7,7 +7,33 @@ const Product = mongoose.model('Product');
 // Objeto
 module.exports = {
     async index(req, res){
-        //listagem de dados
+        const products = await Product.find(); // busca os registros
 
+        return res.json(products);
     },
+
+    async show(req, res){   // Detalhe
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
+    async store(req, res){  // Criação
+        const product = await Product.create(req.body); // req contem todos os campos da requisição
+
+        return res.json(product);
+    },
+
+    async update(req, res){ // Atualização
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true});
+
+        return res.json(product);
+    },
+
+    async destroy (req, res){   // Remoção
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+
+    }
 };
