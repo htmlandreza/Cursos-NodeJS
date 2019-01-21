bbdd5958c8268d1c5501faf64ec1aaaa3264c337
Andreza Moreira <htmlandreza@gmail.com>
2019-01-21 14:09:43 -0200

    Adicionando rotas get, post, put e delete

--- Rocketseat/node-api/src/routes.js
+++ Rocketseat/node-api/src/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-    return res.send("oi");
-});
+const ProductController = require('./controllers/ProductController');
+
+routes.get("/products", ProductController.index);           // listar
+routes.get("/products/:id", ProductController.show);        // detalhes
+routes.post("/products", ProductController.store);          // criar, adicionar
+routes.put("/products/:id", ProductController.update);      // atualizar
+routes.delete("/products/:id", ProductController.destroy);  // destroi, excluir
 
 module.exports = routes;
