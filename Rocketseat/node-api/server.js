aae559f66817bb99ec93542e62890971f5dde51e
Andreza Moreira <htmlandreza@gmail.com>
2019-01-21 14:35:17 -0200

    Adicionando o CORS

--- Rocketseat/node-api/server.js
+++ Rocketseat/node-api/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // Iniciando o APP
 const app = express();
 app.use(express.json());
+app.use(cors()); // pode-se passar domínios específicos pra obter acesso
 
 // Iniciando o Database                     
 mongoose.connect(
